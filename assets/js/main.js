34b8c2b6dad70c337fcab837fbc29d5d1dd74ba5
prakasharul <arulprakash767@gmail.com>
2017-09-23 22:29:26 +0530

    path fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ app.controller('MainCtrl', function($scope, $http) {
 
 	// get post from json
 	$scope.PostLoad = function() {
-		$http.get('../main.json')
+		$http.get('main.json')
 			.then(function(response) {
 				$scope.posts = response.data;
 				console.log(response.data);
