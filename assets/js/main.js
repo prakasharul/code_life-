999ec2b440e455a34f107964eeb11e908f06b630
jeeeth <arulprakash767@gmail.com>
2017-09-23 21:58:05 +0530

    code life static blog

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+var app = angular.module('codelife', []);
+// services
+// main controller
+app.controller('MainCtrl', function($scope, $http) {
+
+	// get post from json
+	$scope.PostLoad = function() {
+		$http.get('../../main.json')
+			.then(function(response) {
+				$scope.posts = response.data;
+				console.log(response.data);
+			});
+	};
+	$scope.PostLoad();
+
+	// grid layout
+	$scope.GridLayout = function() {
+		$("#listlayout").hide('slow');
+		$("#gridlayout").show();
+		$("#")
+	};
+	$scope.GridLayout();
+
+	// list layout
+	$scope.ListLayout = function() {
+		$("#gridlayout").hide('slow');
+		$("#listlayout").show();
+	}
+});
