68c9812d0c77959dda674588abe06bec68ef75af
prakasharul <arulprakash767@gmail.com>
2017-10-02 15:03:05 +0530

    angular routing implemented

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,59 @@
-var app = angular.module('codelife', []); //'ngRoute'
-//config
-// app.config(['$routeProvider', function($routeProvider) {
-// 	$routeProvider.when('/main', {
-// 		templateUrl: 'main.html',
-// 		controller: 'MainCtrl'
-// 	});
-//
-// }]);
+var app = angular.module('codelife', ['ngRoute']); //
+//config -route
+app.config(['$routeProvider', function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'home.html',
+			controller: 'MainCtrl',
+		})
+		.when('/blog', {
+			templateUrl: 'blog.html',
+			controller: 'BlogCtrl',
+		})
+		.when('/contact', {
+			templateUrl: 'contact.html',
+			controller: 'ContactCtrl'
+		})
+		.when('/editor', {
+			templateUrl: 'editor.html',
+			controller: 'EditorCtrl'
+		})
+		.when('/profile', {
+			templateUrl: 'profile.html',
+			controller: 'ProfileCtrl'
+		})
+		.when('/404', {
+			templateUrl: '404.html',
+			controller: 'ErrorCtrl'
+		})
+		.otherwise({
+			redirectTo: '/404'
+		})
+
+
+}]);
 // services
 // main controller
 app.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
+	console.log("home");
+	// grid layout
+	$scope.GridLayout = function() {
+		$("#listlayout").hide('slow');
+		$("#gridlayout").show();
+	};
+	$scope.GridLayout();
 
+	// list layout
+	$scope.ListLayout = function() {
+		$("#gridlayout").hide('slow');
+		$("#listlayout").show();
+	};
+	$scope.ListLayout();
+
+}]);
+//blog controller
+app.controller('BlogCtrl', ['$scope', '$http', function($scope, $http) {
+	console.log("blog");
 	// get post from json
 	$scope.PostLoad = function() {
 		$http.get('main.json')
@@ -21,17 +64,21 @@ app.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
 	};
 	$scope.PostLoad();
 
-	// grid layout
-	$scope.GridLayout = function() {
-		$("#listlayout").hide('slow');
-		$("#gridlayout").show();
-		$("#")
-	};
-	$scope.GridLayout();
 
-	// list layout
-	$scope.ListLayout = function() {
-		$("#gridlayout").hide('slow');
-		$("#listlayout").show();
-	}
 }]);
+// contact controller
+app.controller('ContactCtrl', ['$scope', '$http', function($scope, $http) {
+	console.log("contact");
+}]);
+// editor controller
+app.controller('EditorCtrl', ['$scope', '$http', function($scope, $http) {
+	console.log("editor");
+}]);
+// profile controller
+app.controller('ProfileCtrl', ['$scope', '$http', function($scope, $http) {
+	console.log("profile");
+}]);
+// error controller
+app.controller('ErrorCtrl', ['$scope', '$http', function($scope, $http) {
+	console.log("404");
+}])
